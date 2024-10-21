cc633239609b93ba8177395a8f5c55230c3a9a05
Martin Echenique <martin.echenique@hotmail.com>
2024-10-21 15:48:45 -0300

    Agregue validacion puntaje, no funciona

---
+++ WebMVC/wwwroot/js/validacionesEventos/asignarPuntaje.js
@@ -0,0 +1,17 @@
+﻿window.addEventListener("load", inicial);
+
+function inicial() {
+    document.querySelector("#botonAsignarPuntaje").addEventListener("click", asignarPuntaje);
+}
+
+function asignarPuntaje(p) {
+    p.preventDefault();
+    let puntaje = document.querySelector("#Puntaje").value;
+    let parrafo = document.querySelector("#parrafoPuntaje");
+    let formulario = document.querySelector("#formularioEvento");
+
+    if (puntaje < 0)
+        parrafo.innerHTML = "El puntaje no puede ser menor a 0";
+    else
+        formulario.submit(); 
+}
